b8afe8824eb93bb5567fee36bbe7c3607c5fa00a
Edward Miller <symbiogenesis@outlook.com>
2023-01-08 23:46:13 -0600

    use file-scoped namespace

--- DependencyInjectionTestHelper/DependencyInjectionTestHelper.cs
+++ DependencyInjectionTestHelper/DependencyInjectionTestHelper.cs
@@ -1,3 +1,5 @@
+namespace DependencyInjectionTestHelper;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,174 +7,171 @@ using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace DependencyInjectionTestHelper
+public class DependencyInjectionTestHelper
 {
-    public class DependencyInjectionTestHelper
+    private readonly IServiceCollection _serviceCollection;
+    private readonly IServiceProvider _serviceProvider;
+
+    public DependencyInjectionTestHelper(IWebHostBuilder webHostBuilder)
     {
-        private readonly IServiceCollection _serviceCollection;
-        private readonly IServiceProvider _serviceProvider;
+        IServiceCollection? serviceCollection = null;
 
-        public DependencyInjectionTestHelper(IWebHostBuilder webHostBuilder)
+        webHostBuilder.ConfigureServices(sc => serviceCollection = sc).Build();
+
+        if (serviceCollection == null)
+            throw new InvalidOperationException("Service Collection cannot be null");
+
+        _serviceCollection = serviceCollection;
+
+        var startup = _serviceCollection.BuildServiceProvider().GetRequiredService<IStartup>();
+
+        if (startup == null)
+            throw new InvalidOperationException("Startup class cannot be resolved.");
+
+        _serviceProvider = startup.ConfigureServices(_serviceCollection);
+
+        CheckThatReadonlyMembersAreInitialized(startup, startup.GetType());
+    }
+
+    public void TryToResolveAllServices()
+    {
+        foreach (var descriptor in _serviceCollection.Where(IsNotFromSystemAssembly))
         {
-            IServiceCollection? serviceCollection = null;
+            if (descriptor == null)
+                continue;
 
-            webHostBuilder.ConfigureServices(sc => serviceCollection = sc).Build();
+            if (descriptor.ServiceType == null)
+                throw new InvalidOperationException("Service Type missing");
 
-            if (serviceCollection == null)
-                throw new InvalidOperationException("Service Collection cannot be null");
+            var serviceType = descriptor.ServiceType;
 
-            _serviceCollection = serviceCollection;
+            object service;
 
-            var startup = _serviceCollection.BuildServiceProvider().GetRequiredService<IStartup>();
-
-            if (startup == null)
-                throw new InvalidOperationException("Startup class cannot be resolved.");
-
-            _serviceProvider = startup.ConfigureServices(_serviceCollection);
-
-            CheckThatReadonlyMembersAreInitialized(startup, startup.GetType());
-        }
-
-        public void TryToResolveAllServices()
-        {
-            foreach (var descriptor in _serviceCollection.Where(IsNotFromSystemAssembly))
+            if (descriptor.Lifetime == ServiceLifetime.Singleton)
             {
-                if (descriptor == null)
-                    continue;
+                service = _serviceProvider.GetRequiredService(serviceType);
+            }
+            else
+            {
+                using var scope = _serviceProvider.CreateScope();
+                service = scope.ServiceProvider.GetRequiredService(serviceType);
+            }
 
-                if (descriptor.ServiceType == null)
-                    throw new InvalidOperationException("Service Type missing");
+            if (service == null)
+                throw new InvalidOperationException($"The service {serviceType.Name} was not resolved");
 
-                var serviceType = descriptor.ServiceType;
+            CheckThatReadonlyMembersAreInitialized(service, serviceType);
+        }
+    }
 
-                object service;
+    private static bool IsNotFromSystemAssembly(ServiceDescriptor d)
+    {
+        return IsNotFromSystemAssembly(d?.ServiceType);
+    }
 
-                if (descriptor.Lifetime == ServiceLifetime.Singleton)
+    private static bool IsNotFromSystemAssembly(Type? type)
+    {
+        var fullName = type?.FullName;
+
+        if (fullName == null)
+            return false;
+
+        if (fullName.StartsWith(nameof(Microsoft)))
+            return false;
+
+        if (fullName.StartsWith(nameof(System)))
+            return false;
+
+        return true;
+    }
+
+    private static void CheckThatReadonlyMembersAreInitialized(object service, Type type)
+    {
+        if (type == null)
+            return;
+
+        foreach (var member in GetReadonlyMembers(type))
+        {
+            bool failed;
+
+            var memberType = "unknown member";
+
+            try
+            {
+                switch (member)
                 {
-                    service = _serviceProvider.GetRequiredService(serviceType);
-                }
-                else
-                {
-                    using var scope = _serviceProvider.CreateScope();
-                    service = scope.ServiceProvider.GetRequiredService(serviceType);
-                }
+                    case FieldInfo field:
+                        {
+                            var fieldValue = field.GetValue(service);
+                            failed = fieldValue == null;
+                            memberType = nameof(field);
+                            break;
+                        }
 
-                if (service == null)
-                    throw new InvalidOperationException($"The service {serviceType.Name} was not resolved");
+                    case PropertyInfo property:
+                        {
+                            var propertyValue = property.GetValue(service);
+                            failed = propertyValue == null;
+                            memberType = nameof(property);
+                            break;
+                        }
 
-                CheckThatReadonlyMembersAreInitialized(service, serviceType);
+                    default:
+                        {
+                            failed = true;
+                            break;
+                        }
+                }
+            }
+            catch
+            {
+                failed = true;
+            }
+
+            if (failed)
+            {
+                var errorMessage = $"The {memberType} {member.Name} for service {type.Name} was not resolved";
+                throw new InvalidOperationException(errorMessage);
+            }
+        }
+    }
+
+    private static IEnumerable<MemberInfo> GetReadonlyMembers(Type type)
+    {
+        var members = new List<MemberInfo>();
+
+        var readonlyFields = type.GetFields().Where(f => f.IsInitOnly);
+
+        members.AddRange(readonlyFields);
+
+        foreach (var property in type.GetProperties())
+        {
+            var accessors = property.GetAccessors();
+            if (accessors.Length < 2)
+            {
+                members.Add(property);
             }
         }
 
-        private static bool IsNotFromSystemAssembly(ServiceDescriptor d)
+        return members;
+    }
+
+    public static bool IsAssignableFromGenericType(Type givenType, Type genericType)
+    {
+        foreach (var it in givenType.GetInterfaces())
         {
-            return IsNotFromSystemAssembly(d?.ServiceType);
-        }
-
-        private static bool IsNotFromSystemAssembly(Type? type)
-        {
-            var fullName = type?.FullName;
-
-            if (fullName == null)
-                return false;
-
-            if (fullName.StartsWith(nameof(Microsoft)))
-                return false;
-
-            if (fullName.StartsWith(nameof(System)))
-                return false;
-
-            return true;
-        }
-
-        private static void CheckThatReadonlyMembersAreInitialized(object service, Type type)
-        {
-            if (type == null)
-                return;
-
-            foreach (var member in GetReadonlyMembers(type))
-            {
-                bool failed;
-
-                var memberType = "unknown member";
-
-                try
-                {
-                    switch (member)
-                    {
-                        case FieldInfo field:
-                            {
-                                var fieldValue = field.GetValue(service);
-                                failed = fieldValue == null;
-                                memberType = nameof(field);
-                                break;
-                            }
-
-                        case PropertyInfo property:
-                            {
-                                var propertyValue = property.GetValue(service);
-                                failed = propertyValue == null;
-                                memberType = nameof(property);
-                                break;
-                            }
-
-                        default:
-                            {
-                                failed = true;
-                                break;
-                            }
-                    }
-                }
-                catch
-                {
-                    failed = true;
-                }
-
-                if (failed)
-                {
-                    var errorMessage = $"The {memberType} {member.Name} for service {type.Name} was not resolved";
-                    throw new InvalidOperationException(errorMessage);
-                }
-            }
-        }
-
-        private static IEnumerable<MemberInfo> GetReadonlyMembers(Type type)
-        {
-            var members = new List<MemberInfo>();
-
-            var readonlyFields = type.GetFields().Where(f => f.IsInitOnly);
-
-            members.AddRange(readonlyFields);
-
-            foreach (var property in type.GetProperties())
-            {
-                var accessors = property.GetAccessors();
-                if (accessors.Length < 2)
-                {
-                    members.Add(property);
-                }
-            }
-
-            return members;
-        }
-
-        public static bool IsAssignableFromGenericType(Type givenType, Type genericType)
-        {
-            foreach (var it in givenType.GetInterfaces())
-            {
-                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
-                    return true;
-            }
-
-            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
                 return true;
-
-            var baseType = givenType.BaseType;
-
-            if (baseType == null)
-                return false;
-
-            return IsAssignableFromGenericType(baseType, genericType);
         }
+
+        if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            return true;
+
+        var baseType = givenType.BaseType;
+
+        if (baseType == null)
+            return false;
+
+        return IsAssignableFromGenericType(baseType, genericType);
     }
 }
